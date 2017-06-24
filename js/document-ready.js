f9f8399fa86171972eca4e9514b23f97f83b13af
Dominika Gnich <d.gnich@wp.pl>
2017-06-24 14:24:06 +0200

    ready

---
+++ js/document-ready.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// korzystamy z metody  document.ready(). zeby sprawdzic czy strona jest calkowicie załadowana 
+
+$(function(){
+  
+  
+// alert('test');
+  $('#first').css({color:'red'}).hide(2000).show(3000);
+    
+  });
+
